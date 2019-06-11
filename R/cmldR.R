91dbef6fe2964d4f9e4d332a05122be09ef463da
edsandorf <esandorf@gmail.com>
2019-06-11 13:12:10 +0100

    importFrom stats added

---
+++ R/cmldR.R
@@ -0,0 +1,12 @@
+#' cmdlR: Choice modeling in R
+#' 
+#' cmldR is a package for choice modeling in R. It contains functions to handle 
+#' data input, setup for multicor estimation and several pre-programmed 
+#' likelihood functions that will quickly get you analyzing data.
+#' 
+#' 
+#' @docType package
+#' @name cmldR
+#' 
+#' @importFrom stats runif
+NULL
