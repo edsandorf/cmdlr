8ece1d1c16951b0911a1eb866a266f7d2d4efcfd
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-05 16:11:48 +0000

    Added the detach_all() function

---
+++ R/detach_all.R
@@ -0,0 +1,26 @@
+#' Detach all non-essential objects from all environments
+#' 
+#' This function detaches all non-essential objects from all environments and is 
+#' a simple rewrite of a function available through the codes provided by
+#' the Choice Modeling Centre in Leeds.
+#' 
+#' @examples
+#' detach_all()
+#' 
+#' @export
+
+detach_all <- function () {
+  subset_environment <- substring(search(), 1L, 8L) != "package:" &
+    search() != ".GlobalEnv" & search() != "Autoloads" & 
+    search() != "CheckExEnv" & search() != "tools:rstudio" &
+    search() != "TempEnv"
+  
+  object_positions <- (1L:length(search()))[subset_environment]
+  
+  for (i in 1L:length(object_positions)) {
+    if (length(object_positions) > 0L){
+      detach(pos = object_positions[1L])
+      object_positions <- (1L:length(search()))[subset_environment]
+    }
+  }
+}
