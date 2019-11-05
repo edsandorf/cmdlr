aac2121814d6ff60b29c103bd3eea04b26f0c714
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-05 16:22:12 +0000

    Added placeholder for checking the estimation option

---
+++ R/check_estim_opt.R
@@ -0,0 +1,22 @@
+#' Check estimation options
+#'
+#' The function checks the list of estimation options specified by the user. If
+#' no list of options is supplied or some of the options are not defined, the 
+#' function will supply reasonable default values. The list of estimation options
+#' specifies options related to the optimization routines used.
+#'
+#' @param estim_opt_input A list of user specified estimation options
+#'
+#' @return Returns a list of options with missing input values replaced by 
+#' default values
+
+check_estim_opt <- function(estim_opt_input) {
+  estim_opt <- list(
+    
+  )
+  
+  # Replace the non-specified values with default values
+  estim_opt[names(estim_opt_input)] <- estim_opt_input
+  
+  return(estim_opt)
+}
