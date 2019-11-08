b4f4ca5b41867a5cb42a65df362e44075ad1de9c
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-08 08:17:41 +0000

    Outline for mnl_probs() function

---
+++ R/mnl_probs.R
@@ -0,0 +1,12 @@
+#' Function for calculating the MNL probabilities
+#'
+#' Calculate the MNL probabilities taking the difference from the chosen
+#' alternative.
+#'
+#' @param model_opt List of model options
+#'
+#' @export
+
+mnl_probs <- function(model_opt) {
+  
+}
