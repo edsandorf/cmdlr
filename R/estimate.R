c41da29b5a13c4690ca47bf3eee9f6c9f55b430f
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-09 17:40:10 +0000

    Added outline of estimate function

---
+++ R/estimate.R
@@ -0,0 +1,12 @@
+#' Estimates the model 
+#' 
+#' The function is a wrapper for several cmdlR functions that sets up parallel
+#' processes and estimates the data. 
+#'
+#' @param estim_opt List of estimation options
+#' 
+#' @export
+
+estimate <- function(estim_opt) {
+  
+}
