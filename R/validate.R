8f15589b5ad21d0dbcddb3e11cad07b224d83924
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-09 17:13:42 +0000

    Added skeleton files for the validate function

---
+++ R/validate.R
@@ -0,0 +1,11 @@
+#' Validates the inputs to the model estimation
+#' 
+#' The function is a wrapper for several cmdlR functions that checks the inputs,
+#' controls and data. The purpose is to make sure that the combination of 
+#' inputs are allowed and supported by the package. 
+#'
+#' @export
+
+validate <- function() {
+  
+}
