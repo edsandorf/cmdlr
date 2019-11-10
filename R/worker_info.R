4f5308263af439009e6c08200b24608cde13e863
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-10 17:44:42 +0000

    Added outline of worker_info functions

---
+++ R/worker_info.R
@@ -0,0 +1,24 @@
+#' Function for getting worker info
+#'
+#' The function will loop through the workers to get information about what
+#' objects are loaded on each worker and how the work load is distributed.
+#' 
+#' @param workers A cluster of workers
+#'
+#' @export
+
+get_worker_info <- function(workers) {
+  
+}
+
+#' Function for summarizing worker information
+#' 
+#' The function summarizes and prepares to print the worker information to a
+#' .txt file
+#' 
+#' @param worker_info A worker information object from get_worker_info()
+#'
+#' @export
+summary_worker_info <- function(worker_info) {
+  
+}
