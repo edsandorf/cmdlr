0ea16bcc7a30829a98001929b29f605f809024b8
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-10 17:45:45 +0000

    Added workers and log_lik arguement to estimate function and added attach and on.exit() statements

--- R/estimate.R
+++ R/estimate.R
@@ -3,10 +3,16 @@
 #' The function is a wrapper for several cmdlR functions that sets up parallel
 #' processes and estimates the data. 
 #'
+#' @param log_lik A log likeliohood function to be maximized/minimized
 #' @param estim_opt List of estimation options
+#' @param workers A worker cluster for parallel estimation
 #' 
 #' @export
 
-estimate <- function(estim_opt) {
+estimate <- function(log_lik, estim_opt, workers = NULL) {
+  # Attach the variables to enable calling by names
+  attach()
+  on.exit(detach(), add = TRUE)
   
+  if (estim_opt$cores > 1) on.exit(parallel::stopCluster(workers), add = TRUE)
 }
