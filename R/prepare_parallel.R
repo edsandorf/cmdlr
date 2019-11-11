caa165f5cabcd20685b6d02281740f3e3ec7e61c
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-11 06:25:26 +0000

    Added function arguments and testing the number of cores to the validate function.

--- R/prepare_parallel.R
+++ R/prepare_parallel.R
@@ -4,23 +4,26 @@
 #' objects and functions to the workers.
 #' 
 #' @param estim_opt List of estimation options
+#' @param save_opt List of options for saving outputs
 #' 
 #' @export
 
-prepare_parallel <- function(estim_opt) {
-  cat(crayon::black$bold("Preparing workers ...\n"))
+prepare_parallel <- function(estim_opt, save_opt) {
+  cat(black$bold("Preparing workers ...\n"))
   
   # Prepare the data
   
+  # Create the cluster of workers
+  workers <- parallel::makeCluster(estim_opt$cores, type = "PSOCK")
   
   # Save information about what is loaded on the workers
   if (save_opt$save_worker_info) {
     worker_info <- get_worker_info(workers)
     summary_worker_info(worker_info)
     save_path <- paste0(save_opt$path, "-worker-info.txt")
-    cat(crayon::silver("" %+% crayon::black$bold("Worker information written to: ") %+% save_path))
+    cat(silver("" %+% black$bold("Worker information written to: ") %+% save_path))
   }
   
   # Print exit message if successful
-  cat(crayon::green$bold("Success preparing workers. Estimation starting ...\n"))
+  cat(green$bold("Success: " %+% reset$silver("Workers prepared. Estimation starting ...\n")))
 }
--- R/validate.R
+++ R/validate.R
@@ -4,8 +4,20 @@
 #' controls and data. The purpose is to make sure that the combination of 
 #' inputs are allowed and supported by the package. 
 #'
+#' @param estim_opt List of estimation options
+#' 
 #' @export
 
-validate <- function() {
+validate <- function(estim_opt) {
   
+  # Check that the number of cores are reasonable
+  if (estim_opt$cores > 1) {
+    cat(black("Checking parallel options ..."))
+    if (estim_opt$cores < parallel::detectCores()) {
+      cat(green$bold("Success: " %+% reset$silver("Number of cores OK.")))
+    } else {
+      estim_opt$cores <<- max(1L, parallel::detectCores() - 1L)
+      cat(yellow$bold("Warning: " %+% reset$silver("Cores exceed available. Set to max - 1.")))
+    }
+  }
 }
