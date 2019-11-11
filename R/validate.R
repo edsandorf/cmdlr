fe3266eaa3a7c76077d7fa3397bc9610c1237808
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-11 06:29:52 +0000

    Added line breaks to console output

--- R/validate.R
+++ R/validate.R
@@ -12,12 +12,12 @@ validate <- function(estim_opt) {
   
   # Check that the number of cores are reasonable
   if (estim_opt$cores > 1) {
-    cat(black("Checking parallel options ..."))
+    cat(black("Checking parallel options ...\n"))
     if (estim_opt$cores < parallel::detectCores()) {
-      cat(green$bold("Success: " %+% reset$silver("Number of cores OK.")))
+      cat(green$bold("Success: " %+% reset$silver("Number of cores OK.\n")))
     } else {
       estim_opt$cores <<- max(1L, parallel::detectCores() - 1L)
-      cat(yellow$bold("Warning: " %+% reset$silver("Cores exceed available. Set to max - 1.")))
+      cat(yellow$bold("Warning: " %+% reset$silver("Cores exceed available. Set to max - 1.\n")))
     }
   }
 }
