dd9c0cb7dff1d8280a3b5a8216016e10b6f7c321
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-12 08:39:30 +0000

    Updated function arguments and added set_estimation_opt()

--- R/validate.R
+++ R/validate.R
@@ -5,10 +5,23 @@
 #' inputs are allowed and supported by the package. 
 #'
 #' @param estim_opt List of estimation options
+#' @param model_opt List of model options
+#' @param save_opt List of options for saving outputs
 #' 
 #' @export
 
-validate <- function(estim_opt) {
+validate <- function(estim_opt, model_opt, save_opt) {
+  # Set estimation options
+  estim_opt <- tryCatch({
+    set_estim_opt_defaults(estim_opt)
+  }, warning = function(w) {
+    cat(yellow$bold("Warning: "))
+  }, error = function(e) {
+    cat(red$bold("Error: " %+% reset$silver("Failed to set default options for estim_opt. Check your inputs.")))
+  })
+  cat(green$bold("Success: " %+% reset$silver("Default estimation options set for unspecified options.")))
+  
+  
   
   # Check that the number of cores are reasonable
   if (estim_opt$cores > 1) {
@@ -20,4 +33,7 @@ validate <- function(estim_opt) {
       cat(yellow$bold("Warning: " %+% reset$silver("Cores exceed available. Set to max - 1.\n")))
     }
   }
+  
+  
+  
 }
