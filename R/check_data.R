2b9932f9ed43c3c36569cfb33789e13abc652f35
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-13 13:37:52 +0000

    Added outline for split data functions.

---
+++ R/check_data.R
@@ -0,0 +1,18 @@
+#' Function to check the data
+#'
+#' The function will check the data to see if it is balanced. It will return
+#' a list of conditions indicating which aspects of the data is unbalanced,
+#' which enables the code to call the correct functions to set up the data.
+#' 
+#' @return A list of conditions
+#' 
+#' @param db Data
+#' @param estim_opt List of estimation options
+#' @param model_opt List of model options
+#'
+#' @export
+
+check_data <- function(db, estim_opt, model_opt) {
+  # Check choice tasks
+  
+}
---
+++ R/split_data.R
@@ -0,0 +1,9 @@
+#'
+#'
+#'
+#'
+#'
+
+split_data <- function() {
+  
+}
