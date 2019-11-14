ffd92ab372213965094e691e9bd5c61b6e602451
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-14 20:00:36 +0000

    Split data now takes the arguments of model_opt and estim_opt.

--- R/split_data.R
+++ R/split_data.R
@@ -1,9 +1,15 @@
+#' Function to split the data
 #'
+#' The function takes the data and splits it into roughly equal chunks to be
+#' sent to the workers for parallel processing. The function takes care to not
+#' split the same individual across multiple workers.
+#' 
+#' @param db A data frame
+#' @param estim_opt List of estimation options
+#' @param model_opt List of model options
 #'
-#'
-#'
-#'
+#' @return A list of data matrices with length equal to \code{cores}.
 
-split_data <- function() {
+split_data <- function(db, estim_opt, model_opt) {
   
 }
