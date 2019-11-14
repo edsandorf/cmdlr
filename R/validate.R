c0b531920e00cccead9c0a21ba87fc5a97266a94
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-14 20:01:06 +0000

    Removed unnecessary tryCatch() for validate

--- R/validate.R
+++ R/validate.R
@@ -8,52 +8,62 @@
 #' @param estim_opt List of estimation options
 #' @param model_opt List of model options
 #' @param save_opt List of options for saving outputs
+#' @param log_lik Log likelihood function
 #' 
 #' @export
 
-validate <- function(estim_opt, model_opt, save_opt, db) {
+validate <- function(estim_opt, model_opt, save_opt, db, log_lik) {
   # Set default estimation options
-  estim_opt <- tryCatch({
-    set_estim_opt_defaults(estim_opt)
-    cat(green$bold("Success: " %+% reset$silver("Default options set for estim_opt().")))
-  }, warning = function(w) {
-    cat(yellow$bold("Warning: "))
-  }, error = function(e) {
-    cat(red$bold("Error: " %+% reset$silver("Failed to set default options for estim_opt(). Check your inputs.")))
-  })
-  
+  estim_opt <- set_estim_opt_defaults(estim_opt)
+  cat(green$bold("Success: " %+% reset$silver("Default options set for estim_opt().")))
+
   # Set default model options
-  model_opt <- tryCatch({
-    set_model_opt_defaults(model_opt)
-    cat(green$bold("Success: " %+% reset$silver("Default options set for model_opt().")))
-  }, warning = function(w) {
-    cat(yellow$bold("Warning: "))
-  }, error = function(e) {
-    cat(red$bold("Error: " %+% reset$silver("Failed to set default options for model_opt(). Check your inputs.")))
-  })
+  model_opt <- set_model_opt_defaults(model_opt)
+  cat(green$bold("Success: " %+% reset$silver("Default options set for model_opt().")))
   
   # Set default saving options
-  save_opt <- tryCatch({
-    set_save_opt_defaults(save_opt)
-    cat(green$bold("Success: " %+% reset$silver("Default options set for save_opt().")))
-  }, warning = function(w) {
-    cat(yellow$bold("Warning: "))
-  }, error = function(e) {
-    cat(red$bold("Error: " %+% reset$silver("Failed to set default options for save_opt(). Check your inputs.")))
-  })
+  save_opt <- set_save_opt_defaults(save_opt)
+  if (save_opt$path == file.path(getwd(), "model-01")) {
+    cat(blue$bold("Note: " %+% reset$silver(paste0("Outputs are stored in default location: \"",
+                                                   file.path(getwd(), "model-01"), "\"\n"))))
+  }
+  cat(green$bold("Success: " %+% reset$silver("Default options set for save_opt().")))
   
-  # Check that the number of cores are reasonable
+  #-----------------------------------------------------------------------------
+  # Check the data
+  #-----------------------------------------------------------------------------
+  db <- check_data(db, estim_opt, model_opt)
+  
+  #-----------------------------------------------------------------------------
+  # Check parallel options
+  #-----------------------------------------------------------------------------
   if (estim_opt$cores > 1) {
     cat(black("Checking parallel options ...\n"))
+    
+    # Check the number of cores
     if (estim_opt$cores < parallel::detectCores()) {
       cat(green$bold("Success: " %+% reset$silver("Number of cores OK.\n")))
     } else {
-      estim_opt$cores <<- max(1L, parallel::detectCores() - 1L)
+      estim_opt$cores <- max(1L, parallel::detectCores() - 1L)
       cat(yellow$bold("Warning: " %+% reset$silver("Cores exceed available. Set to max - 1.\n")))
     }
+    
+    # Split the data
+    db <- split_data(db, estim_opt, model_opt)
+    
   }
   
-  # Check the data
-  check_data(db, estim_opt, model_opt)
+
   
+  
+  #-----------------------------------------------------------------------------
+  # Return the list of inputs
+  #-----------------------------------------------------------------------------
+  return(list(
+    estim_opt = estim_opt,
+    model_opt = model_opt,
+    save_opt = save_opt,
+    db = db,
+    log_lik
+  ))
 }
