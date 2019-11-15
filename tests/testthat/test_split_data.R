8027c80c47e91de4c8d54dd8dea9be9bea3f3297
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-15 07:32:02 +0000

    Added test for split_data() to check that individuals are not split across cores.

---
+++ tests/testthat/test_split_data.R
@@ -0,0 +1,29 @@
+context("Test that split_data() doesn't split the same individual on multiple
+        cores.")
+
+test_that("split_data() doesn't split the same individual on multiple cores", {
+  model_opt <- list(id = "id",
+                    ct = "ct")
+  estim_opt <- list(cores = 1)
+  db <- cmdlR::data_coral
+  db <- check_data(db, estim_opt, model_opt)
+  
+  estim_opt <- list(cores = 3)
+  db_split <- split_data(db, estim_opt, model_opt)
+  expect_true(all(!Reduce("%in%", lapply(db_split, function(x) {
+    unique(deframe(x[, model_opt$id]))
+  }))))
+  
+  estim_opt <- list(cores = 5)
+  db_split <- split_data(db, estim_opt, model_opt)
+  expect_true(all(!Reduce("%in%", lapply(db_split, function(x) {
+    unique(deframe(x[, model_opt$id]))
+  }))))
+  
+  estim_opt <- list(cores = 10)
+  db_split <- split_data(db, estim_opt, model_opt)
+  expect_true(all(!Reduce("%in%", lapply(db_split, function(x) {
+    unique(deframe(x[, model_opt$id]))
+  }))))
+  
+})
