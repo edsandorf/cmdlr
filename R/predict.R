e5e320f60f11aad56ed3f71712e801e60d54d043
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-16 09:06:16 +0000

    Added skeleton functions for summary() and predict()

---
+++ R/predict.R
@@ -0,0 +1,12 @@
+#' Predicts "the future"
+#'
+#' This function is a wrapper for several functions that allow you to use your
+#' model results to make predictions. 
+#'
+#' @param model A model opbject returned by \code{estimate}
+#' 
+#' @export
+
+predict <- function(model) {
+  
+}
---
+++ R/summary.R
@@ -0,0 +1,13 @@
+#' Summarizes outputs
+#'
+#' This function is a wrapper for several small summary functions that provide
+#' key summary statistics to help interpret the results of your model. 
+#'
+#' @param model A model opbject returned by \code{estimate}
+#' @param summary_opt A list of summary options
+#' 
+#' @export
+
+summary <- function(model, summary_opt) {
+  
+}
