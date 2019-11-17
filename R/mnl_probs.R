ce368612ceea9e53df66428f34b924485c3b6b5e
Erlend Dancke Sandorf <edsandorf@gmail.com>
2019-11-17 16:26:56 +0000

    Minimial mnl_probs()

--- R/mnl_probs.R
+++ R/mnl_probs.R
@@ -3,10 +3,28 @@
 #' Calculate the MNL probabilities taking the difference from the chosen
 #' alternative.
 #'
+#' @param V A list of the observable part of utility
 #' @param model_opt List of model options
 #'
 #' @export
 
-mnl_probs <- function(model_opt) {
+mnl_probs <- function(V, model_opt) {
   
+  # Extract the utility of the chosen alternative
+  v_chosen <- Reduce("+", lapply(seq_along(V), function(j) {
+    V[[j]] * (model_opt$choice == j)
+  }))
+  
+  # Subtract the utility of the chosen alternative from all utilities
+  V <- lapply(V, function(v) v - v_chosen)
+  
+  # Calculate the exponent and sum of utilities
+  exp_V <- lapply(V, function(v) exp(v))
+  sum_V <- Reduce("+", exp_V)
+  
+  # Calculate the probability of the chosen alt
+  pr_chosen <- 1 / sum_V
+  
+  # Return the probability of the chosen alternative
+  return(pr_chosen)
 }
