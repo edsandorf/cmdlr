39666bab1e6384add67b94fe78aba0f06556d81a
edsandorf <edsandorf@gmail.com>
2020-03-16 15:27:15 +0000

    Explicitly write out the whole LL rather than wrap LL elements in hidden functions

--- examples/mnl.R
+++ examples/mnl.R
@@ -1,40 +1,28 @@
-#-------------------------------------------------------------------------------
-# Tidy the environment prior to loading packages 
-#-------------------------------------------------------------------------------
+# Tidy the environment prior to loading packages ----
 cmdlR::tidy(clean = FALSE)
 
-#-------------------------------------------------------------------------------
-# Load the packages
-#-------------------------------------------------------------------------------
+# Load the packages ----
 pkgs <- c("cmdlR")
 invisible(lapply(pkgs, require, character.only = TRUE))
 
-#-------------------------------------------------------------------------------
-# Load and manipulate the data
-#-------------------------------------------------------------------------------
+# Load and manipulate the data ----
 db <- cmdlR::data_coral
 
-#-------------------------------------------------------------------------------
-# Define the list of saving options
-#-------------------------------------------------------------------------------
+# Define the list of saving options ----
 save_opt <- list(
-  path = file.path("Outputs", "mnl-01"),
+  path = file.path("outputs", "mnl-01"),
   save_summary = TRUE,
   save_model_object = TRUE,
   save_hessian = FALSE,
   save_worker_info = FALSE
 )
 
-#-------------------------------------------------------------------------------
-# Define the list of summary options
-#-------------------------------------------------------------------------------
+# Define the list of summary options ----
 summary_opt <- list(
   robust_se = FALSE 
 )
 
-#-------------------------------------------------------------------------------
-# Define the list of estimation options
-#-------------------------------------------------------------------------------
+# Define the list of estimation options ----
 estim_opt <- list(
   optimizer = "maxLik",
   method = "BFGS",
@@ -42,9 +30,7 @@ estim_opt <- list(
   robust_vcov = TRUE
 )
 
-#-------------------------------------------------------------------------------
-# Define the list of model options
-#-------------------------------------------------------------------------------
+# Define the list of model options ----
 model_opt <- list(
   name = "MNL model",
   description = "A simple MNL model to use as an example.",
@@ -62,57 +48,68 @@ model_opt <- list(
   )
 )
 
-#-------------------------------------------------------------------------------
-# Log likelihood function
-#-------------------------------------------------------------------------------
-log_lik <- function(param) {
+# Log likelihood function ----
+log_lik <- function(param, weights) {
+  # Attach the parameters and data  ----
   
-  # Define the list of utilities 
+  # Define the list of utilities ---- 
   V <- list(
     alt1 = b_cost * cost_1 + b_small * small_1 + b_large * large_1 + b_oil * oil_1 + b_fish * fish_1 + b_habitat + habitat_1,
     alt2 = b_cost * cost_2 + b_small * small_2 + b_large * large_2 + b_oil * oil_2 + b_fish * fish_2 + b_habitat + habitat_2,
     alt3 = b_cost * cost_3 + b_small * small_3 + b_large * large_3 + b_oil * oil_3 + b_fish * fish_3 + b_habitat + habitat_3
   )
   
-  # Calculate the probabilities
-  probs <- mnl_probs(V)
+  # Calculate the probabilities ----
+  # Get the utility of the chosen alternative and subtract it from all utilities
+  v_chosen <- Reduce("+", lapply(seq_along(V), function(j) {
+    V[[j]] * (model_opt$choice == j)
+  }))
   
-  # Calculate the product over the panel
-  pr_seq <- panel_product(probs, S)
+  V <- lapply(V, function(v) v - v_chosen)
   
-  # Average over draws - shouldn't matter for the MNL (check this)
-  lik <- avg_draws(pr_seq, N)
+  # Calculate the exponent and sum of utilities
+  exp_v <- lapply(V, function(v) exp(v))
+  sum_v <- Reduce("+", exp_v)
+  
+  # Calculate the probability of the chosen alt
+  pr_chosen <- 1 / sum_v
+  
+  # Calculate the product over the panel by reshaping to have rows equal to S
+  pr_chosen <- matrix(pr_chosen, nrow = S)
+  
+  # If the data is padded, we need to insert ones before taking the product
+  index_na <- is.na(pr_chosen)
+  pr_chosen[index_na][!is.nan(pr_chosen[index_na])] <- 1
+  
+  # Calculate the probability of the sequence
+  pr_seq <- Rfast::colprods(pr_chosen)
+  
+  # Reshape the matrix such that each row is an individual. Note the stacking
+  pr_seq <- matrix(pr_seq, nrow = N)
+  
+  lik <- Rfast::rowmeans(pr_seq)
   
   # Return the log-likelihood value
-  return(log(lik))
+  log(lik) * weights
 }
 
-#-------------------------------------------------------------------------------
-# Validate the model inputs
-#-------------------------------------------------------------------------------
+# Validate the model inputs ----
 opts <- validate(estim_opt, model_opt, save_opt, summary_opt, log_lik)
 
-#-------------------------------------------------------------------------------
-# Prepare for estimation
-#-------------------------------------------------------------------------------
+# Prepare for estimation ----
 inputs <- prepare(opts, db, log_lik)
 
-#-------------------------------------------------------------------------------
-# Estimate the model
-#-------------------------------------------------------------------------------
+# Estimate the model ----
 model <- estimate(inputs)
 
-#-------------------------------------------------------------------------------
-# Get a summary of the results
-#-------------------------------------------------------------------------------
+# Get a summary of the results ----
 summarize(model, summary_opt)
 
-#-------------------------------------------------------------------------------
-# Make predictions
-#-------------------------------------------------------------------------------
+# Collate results to a single file ----
+#collate() 
+
+# Make predictions ----
 predictions <- predict(model)
 
-#-------------------------------------------------------------------------------
-# Save the results
-#-------------------------------------------------------------------------------
+# Save the results ----
 store()
