ff749991e649927efe477abb64759e3bcf2f2054
edsandorf <edsandorf@gmail.com>
2020-03-16 17:17:57 +0000

    Pad data uses base to avoid additional dependencies

--- R/pad_data.R
+++ R/pad_data.R
@@ -12,14 +12,15 @@ pad_data <- function(db, model_opt) {
   N <- length(unique(db[[model_opt[["id"]]]]))
   S <- length(unique(db[[model_opt[["ct"]]]]))
   
-  ct_tmp <- tibble(X = seq_len(S))
+  ct_tmp <- data.frame(X = seq_len(S))
+  
   names(ct_tmp) <- model_opt[["ct"]]
   
-  db <- lapply(deframe(unique(db[, model_opt[["id"]]])), function(i) {
+  db <- lapply(unique(db[, model_opt[["id"]]]), function(i) {
     db_tmp <- db[db[, model_opt[["id"]]] == i, ]
     if (nrow(db_tmp) != S) {
-      db_tmp <- left_join(ct_tmp, db_tmp, by = model_opt[["ct"]]) %>%
-        mutate(id = i)
+      db_tmp <- merge(ct_tmp, db_tmp, by = model_opt[["ct"]], all.x = TRUE)
+      db_tmp$id <- i
     }
     
     # Return the padded data matrix
