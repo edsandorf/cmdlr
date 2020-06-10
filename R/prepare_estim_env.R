511aba85d67eeca8b0200fdc25663062676ac1f0
edsandorf <edsandorf@gmail.com>
2020-06-10 11:54:43 +0100

    Simplified with list2env()

--- R/prepare_estim_env.R
+++ R/prepare_estim_env.R
@@ -13,7 +13,6 @@
 #' in estimation.
 prepare_estim_env <- function(db, draws, model_opt) {
   estim_env <- rlang::env(
-    # param_fixed = unlist(model_opt$param[model_opt$fixed]),
     N = length(unique(db[[model_opt[["id"]]]])),
     S = length(unique(db[[model_opt[["ct"]]]])),
     J = model_opt$J,
@@ -21,19 +20,11 @@ prepare_estim_env <- function(db, draws, model_opt) {
   )
   
   # Add the data to the estimation environment
-  invisible(
-    lapply(seq_along(db), function(i) {
-      assign(names(db[i]), db[[i]], envir = estim_env)
-    })
-  )
+  list2env(as.list(db), envir = estim_env)
   
   # Add the draws to the estimation environment
   if (model_opt$mixing) {
-    invisible(
-      lapply(seq_along(draws), function(i) {
-        assign(names(draws[i]), draws[[i]], envir = estim_env)
-      })
-    )
+    list2env(as.list(draws), envir = estim_env)
   }
 
   # Return the estimation environment
